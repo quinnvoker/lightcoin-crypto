50ade4fac761a36cb20fdb626d20eff4cdf92076
Quinn <quinnvoker@gmail.com>
2020-07-31 18:08:25 +0000

    added account functionality

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 let balance = 500.00;
 
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
+
 class Withdrawal {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
 
 }
 
 class Deposit {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance += this.amount;
+    this.account.balance += this.amount;
   }
 }
 
@@ -29,16 +39,18 @@ class Deposit {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00);
+const t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
